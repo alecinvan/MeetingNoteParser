f0d93020877844482d897eff823a93be92f6d619
Alec Li <alecinvan@users.noreply.huggingface.co>
2024-01-30 00:25:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -143,6 +143,8 @@ def translate(input_text, inputs_transStyle):
 # 在 https://platform.openai.com/signup 注册并获取 API 密钥
 #openai.api_key = "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"
 
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
 
 def generate_summary(text_input):
     response = openai.ChatCompletion.create(
